507d2765d30731b4cfe81fc106935411a466751c
SimantaDas11 <harekrishna01794322039@gmail.com>
2024-05-19 22:17:21 +0600

    update

--- js/nav-hidden.js
+++ js/nav-hidden.js
@@ -73,19 +73,12 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-// const sr = ScrollReveal({
-//   distance: '60px',
-//   duration: 2500,
-//   delay: 400,
-//   // reset: true
-// })
+const sr = ScrollReveal({
+  distance: '60px',
+  duration: 2500,
+  delay: 100,
+  // reset: true
+})
 
-// sr.reveal(`.home__header, .section__title`, { delay: 600 })
-// sr.reveal(`.home__footer`, { delay: 700 })
-// sr.reveal(`.home__img`, { delay: 900, origin: 'top' })
-
-// sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`, { origin: 'top', interval: 100 })
-// sr.reveal(`.specs__data, .discount__animate`, { origin: 'left', interval: 100 })
-// sr.reveal(`.specs__img, .discount__img`, { origin: 'right' })
-// sr.reveal(`.case__img`, { origin: 'top' })
-// sr.reveal(`.case__data`)
+// sr.reveal(`.big-headings, .main-headings`, { delay: 100, origin: 'top' }) 
+// sr.reveal(`.main-img, .copyright-container`, { delay: 100, origin: 'top' })
