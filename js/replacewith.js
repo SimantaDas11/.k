c8fdb04d663815293adad35e69035129b325feb0
SimantaDas11 <harekrishna01794322039@gmail.com>
2024-05-19 21:15:58 +0600

    update

--- js/replacewith.js
+++ js/replacewith.js
@@ -18,3 +18,23 @@ headerButtons.forEach((button, index) => {
 		mainContainers[index].style.display = 'block';
 	});
 });
+
+const headerButtons2 = document.querySelectorAll("nav div ul li");
+
+const mainContainers2 = document.querySelectorAll('.main-container');
+
+for (let i = 1; i < mainContainers2.length; i++) {
+	mainContainers2[i].style.display = 'none';
+}
+
+
+headerButtons2.forEach((button, index) => {
+	button.addEventListener('click', function() {
+
+		mainContainers2.forEach(container => {
+			container.style.display = 'none';
+		});
+
+		mainContainers2[index].style.display = 'block';
+	});
+});
